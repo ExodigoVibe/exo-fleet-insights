ec775a41a5be04ef013f07d10bb11cb1b2f30976
Ortal Spitzer-Hanoch <Ortal.Spitzer-Hanoch@Ortals-MacBook-Pro.local>
2025-12-02 10:31:45 +0200

    add type

--- src/pages/NewRequest.tsx
+++ src/pages/NewRequest.tsx
@@ -372,7 +372,7 @@ export default function NewRequest() {
                       <FormItem>
                         <FormLabel>Full Name</FormLabel>
                         <FormControl>
-                          <Input placeholder={!user?.full_name ? "Enter your full name" : ""} {...field} />
+                          <Input type="text" placeholder={!user?.full_name ? "Enter your full name" : ""} {...field} />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
